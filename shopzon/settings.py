38caa5a79f58de2aeb767f8206a980c80a353390
aadil611 <ansariaadil611@gmail.com>
2022-08-23 23:01:27 +0530

    changed Celery_broker_url

--- shopzon/settings.py
+++ shopzon/settings.py
@@ -180,7 +180,7 @@ SECURE_CROSS_ORIGIN_OPENER_POLICY='same-origin-allow-popups'
 # Celery Setting
 
 # CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
-CELERY_BROKER_URL = os.environ['REDIS_URL']
+CELERY_BROKER_URL = os.environ.get('REDIS_URL','redis://localhost:6379')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
